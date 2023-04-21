faf8f05abcbf6ca67ed1c80200b5747a2384c68f
Balakhonova Marina <90852680+margoloko@users.noreply.github.com>
2023-04-21 09:34:35 +0300

    Update forms.py
    docstring

--- yacut/forms.py
+++ yacut/forms.py
@@ -4,6 +4,7 @@ from wtforms.validators import DataRequired, Length, Optional, Regexp
 
 
 class UrlForm(FlaskForm):
+    """Форма для создания короткой ссылки."""
     original_link = URLField('Введите URL',
                              validators=[DataRequired('Обязательное поле'),
                                          ])
